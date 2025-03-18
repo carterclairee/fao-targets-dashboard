7ec70693e5dee1389512a13be7b1afcb1f7687e6
Claire Carter <carter.claire.e@gmail.com>
2025-03-18 14:20:18 -0700

    added backend url to apifunctions

--- client/src/pages/APIFunctions.tsx
+++ client/src/pages/APIFunctions.tsx
@@ -1,5 +1,5 @@
 // Checks if app is running locally or on Render and uses correct url accordingly
 export const API_URL = window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://eufmd-targets.onrender.com/";
+    : "https://fao-targets-dashboard-backend.onrender.com";
     
