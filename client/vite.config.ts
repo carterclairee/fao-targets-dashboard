d53d0f465929ca5c168457bd054841a821300898
Claire Carter <carter.claire.e@gmail.com>
2025-03-18 21:28:46 -0700

    revised some config settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      // Resolve plotly.js/dist/plotly to the basic version
-      'plotly.js/dist/plotly': 'plotly.js-basic-dist',
-      'plotly.js': 'plotly.js-basic-dist',
-    },
-  },
   server: {
     proxy: {
       "/api": {
